895e4f47ab69830715b15bd688009f5c1d2eaaf2
jayantpandet77 <jayantpandey.jk@gmail.com>
2025-04-04 16:56:21 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:weather_app/auth/auth_screen.dart';
+import 'package:weather_app/firebase_options.dart';
+import 'package:weather_app/screens/home_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return const HomeScreen();
+          }
+
+          return const AuthScreen();
+        },
+      ),
+    );
+  }
+}
